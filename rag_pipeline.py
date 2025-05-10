40bd1af48218db67953a52af474a8df6d1f0c6f6
Arindam Shukla <arindamshrish@gmail.com>
2025-05-10 21:02:54 +0530

    adding the code for the RAG pipeline

---
+++ rag_pipeline.py
@@ -0,0 +1,57 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import CTransformers
+from langchain.chains import RetrievalQA 
+
+DB_FAISS_PATH='vector_db'
+
+custom_prompt_template = """Use the following pieces of information to answer the user's question. If you don't know the answer, just say so.
+
+Context: {context}
+Question: {question}
+
+Answer:
+"""
+
+def set_custom_prompt():
+    prompt = PromptTemplate(
+        template=custom_prompt_template,
+        input_variables=['context', 'question']
+    )
+    return prompt
+
+def load_llm():
+    llm = CTransformers(
+        model='llama-2-7b-chat.ggmlv3.q8_0.bin',
+        model_type='llama',
+        max_new_tokens=512,
+        temperature=0.5
+    )
+    return llm
+
+def retrieval_qa_chain(llm, prompt, db):
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type='stuff',
+        retriever=db.as_retriever(search_kwargs={'k': 2}),
+        return_source_documents=True,
+        chain_type_kwargs={'prompt': prompt}
+    )
+    return qa_chain
+
+def qa_bot():
+    embeddings = HuggingFaceEmbeddings(
+        model_name='sentence-transformers/all-MiniLM-L6-v2',
+        model_kwargs={'device': 'cpu'}
+    )
+    db = FAISS.load_local(DB_FAISS_PATH, embeddings, allow_dangerous_deserialization=True)
+    llm = load_llm()
+    qa_prompt = set_custom_prompt()
+    qa = retrieval_qa_chain(llm, qa_prompt, db)
+    return qa
+
+def final_result(query):
+    qa = qa_bot()
+    response = qa({'query': query})
+    return response
